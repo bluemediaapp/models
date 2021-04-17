c67d4810efb2e19be025b25f568e43f2be5507e8
Tag-Epic <tagepicuwu@gmail.com>
2021-04-17 16:41:26 +0200

    Revert "Feat: Add per user salt"
    This reverts commit 933126632607fe6caa248328b20ae96caca33f73.

--- models.go
+++ models.go
@@ -4,7 +4,7 @@ package models
 
 type DatabaseVideo struct {
 	// Visible metadata
-	Id          int64  `bson:"_id" json:"id"`
+	Id          int64 `bson:"_id" json:"id"`
 	Description string `bson:"description" json:"description"`
 	Series      string `bson:"series" json:"series"`
 	VideoLength int32  `bson:"video_length" json:"video_length"`
@@ -13,9 +13,9 @@ type DatabaseVideo struct {
 	Likes int64 `bson:"likes"`
 
 	// Backend metadata
-	Tags      []string `bson:"tags" json:"tags"`
-	Modifiers []string `bson:"modifiers" json:"modifiers"`
-	BadTopics int32    `bson:"bad_topics" json:"bad_topics"`
+	Tags          []string `bson:"tags" json:"tags"`
+	Modifiers     []string `bson:"modifiers" json:"modifiers"`
+	BadTopics     int32    `bson:"bad_topics" json:"bad_topics"`
 }
 
 type InitialClassificationResult struct {
@@ -26,7 +26,7 @@ type InitialClassificationResult struct {
 
 type DatabaseUser struct {
 	// Visible metadata
-	Id       int64  `bson:"_id" json:"id"`
+	Id       int64 `bson:"_id" json:"id"`
 	Username string `bson:"username" json:"username"`
 
 	// Backend metadata
@@ -34,20 +34,20 @@ type DatabaseUser struct {
 }
 
 type UserLogin struct {
-	Id       int64  `bson:"id" json:"id"`
+	Id int64 `bson:"id" json:"id"`
 	Username string `bson:"username" json:"username"`
 	Password string `bson:"password" json:"password"`
-	Salt     string `bson:"salt" json:"salt"`
 }
 
 // Generic user interaction
 
 type DatabaseWatchEvent struct {
-	VideoId int64 `bson:"video_id" json:"video_id"`
-	UserId  int64 `bson:"user_id" json:"user_id"`
+	VideoId     int64 `bson:"video_id" json:"video_id"`
+	UserId      int64 `bson:"user_id" json:"user_id"`
 }
 
 type DatabaseLikeEvent struct {
 	VideoId int64 `bson:"video_id" json:"video_id"`
 	UserId  int64 `bson:"user_id" json:"user_id"`
 }
+
